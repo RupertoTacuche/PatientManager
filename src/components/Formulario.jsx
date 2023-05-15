1b3c250b52465182ec174c32bd43a5c88ded748e
RupertoTacuche <murcio2490@gmail.com>
2023-05-14 18:45:05 -0600

    video86

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -160,7 +160,7 @@ const Formulario = ({ patients, setPatients, patient }) => {
           <input 
              type="submit"
              className=" bg-indigo-600 w-full p-3 text-white uppercase font-bold hover:bg-indigo-700 cursor-pointer transition-colors"
-             value="Add patient"
+             value={ patient.id ? 'Edit Patient': 'Add Patient'}
           />
           
         </form>
